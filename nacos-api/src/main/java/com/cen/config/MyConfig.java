199da1b4f1183dd2b99421f91157821213a6ad71
ccBun <2633967166@qq.com>
2023-10-04 23:54:54 +0800

    test rollback

---
+++ nacos-api/src/main/java/com/cen/config/MyConfig.java
@@ -0,0 +1,12 @@
+package com.cen.config;
+
+public class MyConfig {
+
+    public void m1() {
+        System.out.println("step one from ccBun");
+        System.out.println("step two from ccBun");
+        System.out.println("step three from ccBun");
+        System.out.println("step four from ccBun");
+        System.out.println("step five from ccBun");
+    }
+}
