17ac99dac4d8356d8cd3b7b4f1c324a286a8972f
cen <2633957166@qq.com>
2023-10-05 00:11:03 +0800

    test rb

--- nacos-api/src/main/java/com/cen/config/MyConfig.java
+++ nacos-api/src/main/java/com/cen/config/MyConfig.java
@@ -5,7 +5,7 @@ public class MyConfig {
     public void m1() {
         System.out.println("step one from ccBun");
         System.out.println("step two from ccBun");
-        System.out.println("step three from ccBun");
+        System.out.println("step three-2 from cen");
         System.out.println("step four from ccBun");
         System.out.println("step five from ccBun");
     }
